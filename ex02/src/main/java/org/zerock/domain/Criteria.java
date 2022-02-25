e6581cac2dc31dac8c6fd3704729916be15d3b30
phj1120 <hyeonj1998@naver.com>
2022-02-25 22:13:31 +0900

    2022.02.25.

--- ex02/src/main/java/org/zerock/domain/Criteria.java
+++ ex02/src/main/java/org/zerock/domain/Criteria.java
@@ -6,8 +6,24 @@ import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Criteria {
 	private int pageNum;
 	private int amount;
+	
+	private String type;
+	private String keyword;
+	
+	public Criteria() {
+		this(1,10);
+	}
+
+	public Criteria(int pageNum, int amount) {
+		this.pageNum = pageNum;
+		this.amount = amount;
+	}
+	
+	public String[] getTypeArr() {
+		return type==null ? new String[] {} : type.split("");
+	}
+	
 }
